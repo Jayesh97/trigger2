3d4d81c6db69778d305ebd401be7bb3035b82513
Jayesh <jayesh5397@gmail.com>
2019-11-08 12:38:02 -0500

    final

--- jenkins_call.js
+++ jenkins_call.js
@@ -32,7 +32,7 @@ function post_to_botwa(current_build_number, current_build_status)
     var options = getBotwaOptions();
     //console.log(current_build_number);
     //console.log(options);
-    request.post(botwa_config.url, { json: [{build_no:"1"},{build_status:current_build_status}] }, function(error, response, body){
+    request.post(botwa_config.url, { json: {build_no:"1",build_status:current_build_status} }, function(error, response, body){
         if (error) {
             console.error(error)
             return
