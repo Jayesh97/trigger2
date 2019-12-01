77eae2cd403e20da2dd847e4e9dc48d6c92ca5da
jayesh <jayesh5397@gmail.com>
2019-12-01 06:53:57 -0500

    h

--- jenkins_call.js
+++ jenkins_call.js
@@ -3,13 +3,13 @@
 //"116377b0664c6134c003dca90ab543d280"
 var request = require('request');
 
-var jenkins_config = {username:"admin",token:"116377b0664c6134c003dca90ab543d280",project_name:'se_p'}
-var botwa_config = {url:"http://162.222.180.32:3000/complete"}
+var jenkins_config = {username:"admin",token:"1163aa0b65f2bf976134a42ec99f51b720",project_name:'se_project'}
+var botwa_config = {url:"http://34.67.233.131:3000/complete"}
 
 function getDefaultOptions(jobname,endpoint)
 {
 	var options = {
-        url: "http://162.222.180.32:8080/job/"+jobname+"/api/"+endpoint,
+        url: "http://34.67.233.131:8080/job/"+jobname+"/api/"+endpoint,
         auth:{
             user : jenkins_config.username,
             password : jenkins_config.token
@@ -27,11 +27,17 @@ function getBotwaOptions()
     return options;
 }
 
+function getJobsnames()
+{
+    var options = getBotwaOptions();
+    
+}
+
 function post_to_botwa(current_build_number, current_build_status)
 {
     var options = getBotwaOptions();
     //console.log(current_build_number);
-    //console.log(options);
+    console.log(options);
     request.post(botwa_config.url, { json: {build_no:"1",build_status:current_build_status} }, function(error, response, body){
         if (error) {
             console.error(error)
@@ -47,16 +53,17 @@ async function get_job(jobname,endpoint)
     request(options, function(error, response, body){
         //console.log(options)
         if (!error && response.statusCode == 200) {
-            //console.log(body)
+            console.log(body)
             obj = JSON.parse(body);
             current_build_number = obj.lastBuild.number;
             current_build_status = obj.color;
             //console.log(current_build_status)
             //console.log(obj.lastBuild.url);
-            // console.log(obj) // Print the json
+            console.log(obj) // Print the json
             post_to_botwa(current_build_number,current_build_status);
           }
         });
 }
+
 
 get_job(jenkins_config.project_name,"json?pretty=true");
